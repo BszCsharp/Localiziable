b3c83a142c2c2c454441d5d1c9464a98831117e1
regu_tab <GuetlingReinhold@dv-schulen.de>
2016-06-19 23:19:26 +0200

    Datum

--- Localiziable/Form1.cs
+++ Localiziable/Form1.cs
@@ -21,7 +21,6 @@ namespace Localiziable
         {
             toolStripStatusLabel1.Text = Meldungen.ErfolgSpeichern;
             textBoxAnr.Text = "falsche  Meldung";
-            textBoxDatum.Text = DateTime.Now.ToString();
         }
     }
 }
