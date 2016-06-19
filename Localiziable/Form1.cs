641222341d0b83dda39c9fb0d5d6bdfe4a73d883
reinhold <reinhold@web.de>
2016-06-20 00:01:36 +0200

    V4 Text 4

--- Localiziable/Form1.cs
+++ Localiziable/Form1.cs
@@ -20,7 +20,7 @@ namespace Localiziable
         private void button1_Click(object sender, EventArgs e)
         {
             toolStripStatusLabel1.Text = Meldungen.ErfolgSpeichern;
-            textBoxAnr.Text = "Text 3";
+            textBoxAnr.Text = "Text 4";
             //textBoxDatum.Text = DateTime.Now.ToString();
         }
     }
